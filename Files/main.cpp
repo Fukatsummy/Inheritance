89d090c1815f7dfa30da4c16494b18c6734c6d9f
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-06-13 22:04:09 +0300

    Files1

---
+++ Files/main.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	std::ofstream fout;   //Создаем поток
+	fout.open("File.txt",std::ios_base::app); // Открываем поток
+	fout << "Hello World!\n";// Используем поток/Выводим в поток
+	fout.close();// Закрываем поток
+
+	system("notepad File.txt");
+}
