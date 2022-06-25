b7e81505b6cedc73c4fae69531fd40538cbaf8a7
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-06-25 21:09:25 +0300

    line_width and position DONE

--- AbstractGeometry/mail.cpp
+++ AbstractGeometry/mail.cpp
@@ -1,7 +1,11 @@
 ﻿#pragma warning(disable:4326)//убирает ошибку замены main на void
+#define _USE_MATH_DEFINES
 #include<iostream>
 #include<Windows.h>
+#include<math.h>
 using namespace std;
+
+//#define CIRCLE
 // enum (Enumeration - перечисление) -это набор целочисленных констант
 //перечисления так же является типом данных
 namespace Geometry
@@ -11,6 +15,7 @@ namespace Geometry
 		red = 0x000000FF,
 		green = 0x0000FF00,
 		blue = 0x00FF0000,
+		yellow=0x0000FFFF,
 
 		console_default = 0x07,
 		console_blue = 0x99,
@@ -20,18 +25,66 @@ namespace Geometry
 		console_white = 0xFF
 	};
 
+	enum Defaults
+	{
+		min_start_x = 10,
+		max_start_x = 800,
+		min_start_y = 10,
+		max_start_y = 500,
+		min_line_width = 5,
+		max_line_width = 20
+	};
+
 	class Shape
 	{
 	protected:
 		Color color;
+		int start_x;
+		int start_y;
+		unsigned int line_width;
 	public:
 		Color get_color()const
 		{
 			return color;
 		}
+		int get_start_x()const
+		{
+			return start_x;
+		}
+		int get_start_y()const
+		{
+			return start_y;
+		}
+		unsigned int get_line_width()const
+		{
+			return line_width;
+		}
+		void set_start_x(int  start_x)
+		{
+			if (start_x < Defaults::min_start_x)this->start_x = Defaults::min_start_x;
+			else if (start_x > Defaults::max_start_x)this->start_x = Defaults::max_start_x;
+			else this->start_x = start_x;
+		}
+		void set_start_y(int  start_y)
+		{
+			if (start_y < Defaults::min_start_y)this->start_y = Defaults::min_start_x;
+			else if (start_y > Defaults::max_start_y)this->start_y = Defaults::max_start_x;
+			else this->start_y = start_y;
+		}
+		void set_line_width(unsigned int line_width)
+		{
+			if (line_width > Defaults::min_line_width)this->line_width = Defaults::min_line_width;
+			else if (line_width > Defaults::max_line_width)this->line_width = Defaults::max_line_width;
+			else this->line_width = line_width;
+		}
 		
 
-		Shape(Color color) :color(color) {}
+		Shape(int start_x, int start_y, unsigned int line_width, Color color) :color(color)
+		{
+			set_start_x(start_x);
+			set_start_y(start_y);
+			set_line_width(line_width);
+		}
 		virtual ~Shape() {}
 
 		virtual double get_area()const = 0;
@@ -59,7 +112,8 @@ namespace Geometry
 			if (side <= 0)side = 10;
 			this->side = side;
 		}
-		Square(double side, Color color) :Shape(color)
+		Square(double side, int start_x, int start_y, unsigned int line_width, Color color)
+			:Shape(start_x,start_y,line_width,color)
 		{
 			set_side(side);
 		}
@@ -117,7 +171,8 @@ namespace Geometry
 			if (b <= 0)b = 20;
 			this->side_b = side_b;
 		}
-		Rectangle(double side_a, double side_b, Color color) :Shape(color)
+		Rectangle(double side_a, double side_b, int start_x, int start_y, unsigned int line_width, Color color)
+			:Shape(start_x,start_y,line_width,color)
 		{
 			set_side_a(side_a);
 			set_side_b(side_b);
@@ -139,7 +194,7 @@ namespace Geometry
 			//2 Для того чтобы рисовать. нужно создать контекст устройства
 			HDC hdc = GetDC(hwnd);  //HDC(Handler to Device Context) это то на чем мы  будем рисовать
 			//3  Слздадим инструменты, которыми мы будем рисоать
-			HPEN hPen = CreatePen(PS_SOLID, 10, color);//Карандаш рисует контур фигуры
+			HPEN hPen = CreatePen(PS_SOLID, line_width, color);//Карандаш рисует контур фигуры
 			//Для того чтобы применить заливку, нужна кисть
 			HBRUSH hBrush = CreateSolidBrush(color);
 
@@ -148,7 +203,7 @@ namespace Geometry
 			SelectObject(hdc, hPen);
 			SelectObject(hdc, hBrush);
 			//5 Рисуем прямоугольник
-			::Rectangle(hdc, 100, 100, 300, 200);
+			::Rectangle(hdc, start_x, start_y, start_x+side_a, start_y+side_b);
 
 			DeleteObject(hPen);
 			DeleteObject(hBrush);
@@ -162,6 +217,7 @@ namespace Geometry
 			Shape::info();
 		}
 	};
+
 	class Circle :public Shape
 	{
 		double radius;
@@ -173,44 +229,90 @@ namespace Geometry
 		}
 		void set_radius(double radius)
 		{
-			if (radius <= 0)radius = 10;
-			this->radius = radius;
+			if (radius >= 10 && radius <= 500)this->radius = radius;
+			else if (radius < 10)this->radius = 10;
+			else this->radius = 500;
 		}
+#ifdef CIRCLE
 		Circle(double radius, Color color) :Shape(color)
 		{
 			set_radius(radius);
 		}
 		~Circle() {}
+
+		double get_area()const
+		{
+			return 3.14*(radius*radius);
+		}
+		double get_perimeter()const
+		{
+			return 3.14*(radius) * 2;
+		}
+		void draw()const
+		{
+			HWND hConsole = GetConsoleWindow(); //Окно консоли для обращения
+			HDC hdc = GetDC(hConsole);   //Создание отдельного компонента(контекста)
+			HPEN hPen = CreatePen(PS_SOLID, 10, color); //Создание карандаша v
+			HBRUSH hBrush = CreateSolidBrush(color);  //Создание кисти
+			SelectObject(hdc, hPen);   //Выбираем созданный карандаш
+			SelectObject(hdc, hBrush); //Выбираем созданую кисть
+			::Ellipse(hdc, 100, 10, 150, 60);
+			DeleteObject(hPen);
+			DeleteObject(hBrush);
+			ReleaseDC(hConsole, hdc);
+		}
+		void info()const
+		{
+			cout << typeid(*this).name() << endl;
+			cout << "Радиус: " << radius << endl;
+			Shape::info();
+		}
+#endif // CIRCL
 		
-			double get_area()const
-			{
-				return 3.14*(radius*radius);
-			}
-			double get_perimeter()const
-			{
-				return 3.14*(radius) * 2;
-			}
-			void draw()const
-			{
-				HWND hConsole = GetConsoleWindow(); //Окно консоли для обращения
-				HDC hdc = GetDC(hConsole);   //Создание отдельного компонента(контекста)
-				HPEN hPen = CreatePen(PS_SOLID, 10, color); //Создание карандаша v
-				HBRUSH hBrush = CreateSolidBrush(color);  //Создание кисти
-				SelectObject(hdc, hPen);   //Выбираем созданный карандаш
-				SelectObject(hdc, hBrush); //Выбираем созданую кисть
-				::Ellipse(hdc, 0, 0, 50, 50);
-				DeleteObject(hPen);
-				DeleteObject(hBrush);
-				ReleaseDC(hConsole, hdc);
-			}
-			void info()const
-			{
-				cout << typeid(*this).name() << endl;
-				cout << "Радиус: " << radius << endl;
-				Shape::info();
-			}
-		
+	
+		Circle(double radius, int start_x, int start_y, unsigned int line_width, Color color)
+			:Shape(start_x,start_y,line_width,color)
+		{
+			set_radius(radius);
+		}
+		~Circle(){}
+		double get_area()const
+		{
+			return M_PI * radius*radius;
+		}
+		double get_perimeter()const
+		{
+			return M_PI * radius * 2;
+		}
+		void draw()const
+		{
+			HWND hConsole = GetConsoleWindow();
+			HDC hdc = GetDC(hConsole);
+			HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+			HBRUSH hBrush = CreateSolidBrush(color);
+
+			SelectObject(hdc, hPen);
+			SelectObject(hdc, hBrush);
+
+			::Ellipse(hdc, start_x, start_y, start_x+radius*2, start_y+radius*2);
+
+			DeleteObject(hPen);
+			DeleteObject(hBrush);
+			ReleaseDC(hConsole, hdc);
+		}
+		void info()const
+		{
+			cout << typeid(*this).name() << endl;
+			cout << "Радиус: " << radius << endl;
+			Shape::info();
+		}
+
 	};
+	class Poligon
+	{
+
+	};
+
 }
 
 
@@ -219,16 +321,19 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	//Shape shape(Color::console_blue);
-	Geometry::Square square(5, Geometry::Color::console_red);
+	Geometry::Square square(5,50,10,5, Geometry::Color::console_red);
 	/*cout << "Длина стороны квадрата: " << square.get_side() << endl;
 	cout << "Площадт квадрата:       " << square.get_area() << endl;
 	cout << "Периметр кадрата:       " << square.get_perimeter() << endl;
 	square.draw();*/
 	square.info();
 
-	Geometry::Rectangle rect(50, 30, Geometry::Color::blue);
+	Geometry::Rectangle rect(150, 200,200,100,5, Geometry::Color::blue);
 	rect.info();
 
-	Geometry::Circle circle(50, Geometry::Color::green);
+	/*Geometry::Circle circle(50, Geometry::Color::green);
+	circle.info();*/
+
+	Geometry::Circle circle(150, 500,150,15, Geometry::Color::yellow);
 	circle.info();
 }
--- NULLTerminatedLines/main.cpp
+++ NULLTerminatedLines/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int StringLength(const char str[]);
+void to_upper(char str[]);
+void to_lower(char st[]);
 
 void main()
 {
@@ -10,13 +12,25 @@ void main()
 	//system("chcp 1251");
 	const int SIZE = 256;
 	char str[SIZE] = {};
-	cout << " : ";
+	cout << "Введите строку : ";
 	SetConsoleCP(1251);
 	cin.getline(str, SIZE);
 	SetConsoleCP(886);
-	cout << str << endl;
+	//cout << str << endl;
 	//cout << sizeof(str) << endl;
-	cout << " : " << StringLength(str) << endl;
+	//cout << " : " << StringLength(str) << endl;
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] == str[i + 1] == 32)
+		{
+			do {
+				str[i] = str[i + 1];
+				i++;
+			} while (str[i] == 32 && str[i + 1] != 32);
+			
+		}
+		cout << str[i];
+	}
 }
 
 int StringLength(const char str[])
@@ -24,4 +38,19 @@ int StringLength(const char str[])
 	int i = 0;
 	for (; str[i] != 0; i++);
 	return i;
+}
+void to_upper(char str[])
+{
+	for (int i = 0; i < str[i]; i++)
+	{
+		str[i] >= 'а'&&str[i] <= 'я' ? str[i] -= 32 : str[i];
+		str[i] >= 'А'&&str[i] <= 'Я' ? str[i] -= 32:str[i];
+	}
+}
+void to_lower(char str[])
+{
+	for (int i = 0; i < str[i]; i++)
+	{
+		str[i] >= 65 && str[i] <= 90 ? str[i] += 32 : str[i];
+	}
 }
